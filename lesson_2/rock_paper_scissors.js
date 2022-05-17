a222d1e9a946e5018386e1805fcfeaa196631cb4
Blake Fromkin <blakefromkin@gmail.com>
2022-05-17 17:25:38 -0400

    Updated RPS per feedback

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -2,7 +2,8 @@ const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors'];
 let scoreboard = {
   user: 0,
-  computer: 0
+  computer: 0,
+  matchWin: false
 };
 
 function prompt(message) {
@@ -16,14 +17,14 @@ function keepScore(winner) {
   } else if (winner === 'comp') {
     scoreboard.computer += 1;
   }
-
   // Reset after one side reaches 3 wins
   if (Object.values(scoreboard).includes(3)) {
     scoreboard.user = 0;
     scoreboard.computer = 0;
-    return true;
+    scoreboard.matchWin = true;
+  } else {
+    scoreboard.matchWin = false;
   }
-  return false;
 }
 
 function userWins(choice, computerChoice) {
@@ -33,17 +34,21 @@ function userWins(choice, computerChoice) {
 }
 
 function displayWinner(choice, computerChoice) {
+  console.clear();
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
   if (userWins(choice, computerChoice)) {
-    prompt(keepScore('user') ? 'You won the match!' : 'You win the game!');
+    keepScore('user');
+    prompt(scoreboard.matchWin ? 'You won the match!' : 'You win the game!');
   } else if (choice === computerChoice) {
     prompt("It's a tie!");
   } else {
-    prompt(keepScore('comp') ?
-    'Computer won the match!' : 'Computer wins the game!');
+    keepScore('comp');
+    prompt(scoreboard.matchWin ?
+      'Computer won the match!' : 'Computer wins the game!');
   }
 }
+
 
 while (true) {
   // Get user's choice
@@ -60,18 +65,19 @@ while (true) {
   VALID_CHOICES[Math.floor(Math.random() * VALID_CHOICES.length)];
 
   // Display results
-  console.clear();
   displayWinner(choice, computerChoice);
 
 
   // Prompt to play again
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
+  while (answer !== 'n' && answer !== 'y') {
+    prompt('Please enter "y" for yes or "n" for no.');
     answer = readline.question().toLowerCase();
   }
 
-  if (answer[0] !== 'y') break;
+  if (answer[0] !== 'y') {
+    break;
+  }
   console.clear();
 }
